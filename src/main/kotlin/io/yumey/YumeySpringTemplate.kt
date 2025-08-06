7a19864e2d3f2147e3a2f2af96935132f891b6d1
NilsSchlegel08 <janosch.schlegel@gmail.com>
2025-08-06 08:10:55 +0200

    Renamed main Class

--- src/main/kotlin/io/yumey/YumeySpringTemplate.kt
+++ src/main/kotlin/io/yumey/YumeySpringTemplate.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class YumeySpringTemplateApplication
+class YumeySpringTemplate
 
 fun main(args: Array<String>) {
-    runApplication<YumeySpringTemplateApplication>(*args)
+    runApplication<YumeySpringTemplate>(*args)
 }
--- src/test/kotlin/io/yumey/YumeySpringTemplateTests.kt
+++ src/test/kotlin/io/yumey/YumeySpringTemplateTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class YumeySpringTemplateApplicationTests {
+class YumeySpringTemplateTests {
 
     @Test
     fun contextLoads() {

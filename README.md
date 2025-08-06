560af10eb2c39f5df3aa405864b9e2a3f5315e16
NilsSchlegel08 <janosch.schlegel@gmail.com>
2025-08-06 07:56:20 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Yumey Spring Boot Template
---
+++ src/test/kotlin/io/yumey/YumeySpringTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.yumey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YumeySpringTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
